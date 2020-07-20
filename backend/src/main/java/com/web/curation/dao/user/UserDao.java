dfd789b171789006c9e777e7b330b34524a0f2b4
윤상목 <ysm00813@gmail.com>
2020-07-20 10:11:05 +0900

    Backend 기본파일

---
+++ backend/src/main/java/com/web/curation/dao/user/UserDao.java
@@ -0,0 +1,25 @@
+
+package com.web.curation.dao.user;
+
+import java.util.Optional;
+
+import com.web.curation.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao {
+    User getUserByEmail(String email);
+	User getUserByUid(String uid);
+	int insert(User user);
+	int update(User user);
+    User login(User user);
+    User findUserByUidAndPassword(User user);   
+}
+
+//public interface UserDao extends JpaRepository<User, String> {
+//    User getUserByEmail(String email);
+//	User getUserByUid(String uid);
+//	User save(User user);
+//    Optional<User> findUserByEmailAndPassword(String email, String password);
+//    Optional<User> findUserByUidAndPassword(String email, String password);
+//    
+//}
