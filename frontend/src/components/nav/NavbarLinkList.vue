d2d7bca596d3c43529960380c5e61ff50ac65361
JAESUNG <cjs1216@gmail.com>
2020-07-20 17:27:48 +0900

    [S03P12D106-18] 로그인 모달 제작

--- frontend/src/components/nav/NavbarLinkList.vue
+++ frontend/src/components/nav/NavbarLinkList.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="navbar-nav">
       <NavbarLinkListItem value="SignUp" />
-      <NavbarLinkListItem value="Login" />
+      <NavbarLinkListItem value="Login" @click.native="changeLogin"/>
+      <LoginModal v-if="loginModal" @close="changeLogin">
+      </LoginModal>
   </div>
 </template>
 
 <script>
 import NavbarLinkListItem from '@/components/nav/NavbarLinkListItem.vue'
+import LoginModal from '@/components/modal/LoginModal.vue'
 
 export default {
   name: 'NavbarLinkList',
+  data() {
+    return {
+      loginModal: false
+    }
+  },
   components: {
     NavbarLinkListItem,
-  }
+    LoginModal,
+  },
+  methods: {
+    changeLogin(){
+      console.log(this.loginModal)
+      this.loginModal = !this.loginModal
+      console.log(this.loginModal)
+    }
+  },
 }
 </script>
 
