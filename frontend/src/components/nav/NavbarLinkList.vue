25e49976e72a2c842d936410e07826a3a943427a
JAESUNG <cjs1216@gmail.com>
2020-07-22 17:03:55 +0900

    [S03P12D106-24] 비밀번호 찾기 모달 및 임시비밀번호 발급

--- frontend/src/components/nav/NavbarLinkList.vue
+++ frontend/src/components/nav/NavbarLinkList.vue
@@ -2,41 +2,41 @@
   <div class="navbar-nav">
       <NavbarLinkListItem value="SignUp" />
       <NavbarLinkListItem value="Login" @click.native="changeLogin"/>
-      <LoginModal v-if="loginModal" @close="changeLogin" @lost="changeModal">
+      <LoginModal v-if="loginModal" @close="changeLogin" @change="changeModal">
       </LoginModal>
-      <LostPasswordModal v-if="lostPasswordModal" @close="changeLost">
-      </LostPasswordModal>
+      <ForgotPasswordModal v-if="forgotPasswordModal" @close="changeForgot" @change="changeModal">
+      </ForgotPasswordModal>
   </div>
 </template>
 
 <script>
 import NavbarLinkListItem from '@/components/nav/NavbarLinkListItem.vue'
 import LoginModal from '@/components/modal/LoginModal.vue'
-import LostPasswordModal  from '@/components/modal/LostPasswordModal.vue'
+import ForgotPasswordModal  from '@/components/modal/ForgotPasswordModal.vue'
 
 export default {
   name: 'NavbarLinkList',
   data() {
     return {
       loginModal: false,
-      lostPasswordModal: false
+      forgotPasswordModal: false
     }
   },
   components: {
     NavbarLinkListItem,
     LoginModal,
-    LostPasswordModal,
+    ForgotPasswordModal,
   },
   methods: {
     changeLogin () {
       this.loginModal = !this.loginModal
     },
-    changeLost () {
-      this.lostPasswordModal = !this.lostPasswordModal
+    changeForgot () {
+      this.forgotPasswordModal = !this.forgotPasswordModal
     },
     changeModal () {
       this.loginModal = !this.loginModal
-      this.lostPasswordModal = !this.loginModal
+      this.forgotPasswordModal = !this.loginModal
     }
   },
 }
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,13 +3,26 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+// const session = this.$session
 export default new Vuex.Store({
   state: {
+    isLoggedIn: false,
   },
   mutations: {
+    login(state) {
+      state.isLoggedIn = true
+    },
+    logout(state) {
+      state.isLoggedIn = false
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    isLoggedIn(state) {
+      return state.isLoggedIn
+    }
   }
 })
