444537b913107e463a4161f79fbad929aaf3f996
박지훈 <pjh5884@naver.com>
2020-07-24 18:04:36 +0900

    [] 서비스 모달 완성

--- frontend/src/components/modal/ServiceAddModal.vue
+++ frontend/src/components/modal/ServiceAddModal.vue
@@ -35,8 +35,8 @@
                     </select>
                      </div>
                         <div class="input-box font-notojp">
-                      <label class="input-label" for="inputname">가격</label>
-                      <input type="text" id="inputname" placeholder="가격을 입력하세요." v-model="servicedata.price">
+                      <label class="input-label" for="price">가격</label>
+                      <input type="text" id="price" placeholder="가격을 입력하세요." v-model="servicedata.price">
                     </div>
                 </slot>
             </div>
@@ -93,9 +93,18 @@ name: 'ServieAddModal',
     display: table-cell;
     vertical-align: middle;
   }
-
+   @media (min-width: 768px) and (max-width: 999px) {
+    .modal-container {
+         width: 250px;
+    }
+  }
+  @media (min-width: 1000px) {
+    .modal-container {
+    width: 700px;
+     }
+  }
   .modal-container {
-    width: 300px;
+
     margin: 0px auto;
     padding: 20px 30px;
     background-color: #fff;
@@ -103,7 +112,7 @@ name: 'ServieAddModal',
     box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
     transition: all .3s ease;
     font-family: Helvetica, Arial, sans-serif;
-  }
+     }
 
   .modal-header h3 {
     margin-top: 0;
@@ -141,10 +150,9 @@ name: 'ServieAddModal',
     transform: scale(1.1);
   }
 .search-info {
-          
-            margin: 10px 5px 10px 5px;
-            border-radius: 7px;
-            height: 50px;
+    margin: 10px 5px 10px 5px;
+    border-radius: 7px;
+    height: 50px;
         }
         .input-box input {
     width: 100%;
