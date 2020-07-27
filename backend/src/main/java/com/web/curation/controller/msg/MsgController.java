a809033b40e20c565242831469092079cbe0d62b
윤상목 <ysm00813@gmail.com>
2020-07-27 17:06:25 +0900

    전반적 코드 삭제 및 수정

--- backend/src/main/java/com/web/curation/controller/msg/MsgController.java
+++ backend/src/main/java/com/web/curation/controller/msg/MsgController.java
@@ -2,12 +2,15 @@ package com.web.curation.controller.msg;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.curation.service.msg.MsgService;
 
 @CrossOrigin(origins = { "*" })
 @RestController
+@RequestMapping("/msg")
 public class MsgController {
 	
 	@Autowired
