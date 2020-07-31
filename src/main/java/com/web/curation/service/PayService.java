c4f61dc887931f6a3eff2253b9d359adc5f5ef8e
허진기 <god85a@naver.com>
2020-07-31 12:16:49 +0900

    backend 중간

--- src/main/java/com/web/curation/service/PayService.java
+++
@@ -1,8 +0,0 @@
-package com.web.curation.service;
-
-import com.web.curation.model.Pay;
-
-public interface PayService {
-	public boolean insert(Pay pay);
-	public Pay serchPayed(int userno, int servno);
-}
