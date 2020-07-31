81d5b57e082e9f7be3ca4a90486e569b1e9efe64
박지훈 <pjh5884@naver.com>
2020-07-31 16:31:22 +0900

    Service Test

---
+++ frontend/src/views/services/ServiceAdd.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <h3>서비스 등록</h3>
+        <hr>
+        <div>
+            <label>분류</label>
+            <select name ="category">
+                <option value="" v-model:>
+
+        </div>
+    </div>
+</template>
+<script>
+import URL from "@/util/http-common.js"
+import Axios from 'axios'
+
+export default {
+    name : 'ServiceAdd',
+    components : {
+
+    },
+    data(){
+        return {
+            Category:[
+                {
+                    cateno:"",
+                    cname:""
+                }
+            ],
+
+        }
+
+    },
+     created() {
+         axios.get('${URL.BASE_URL}/fselect/cate') 
+          .then(res => {
+            console.log(res)
+            this.Category = {
+                cateno:  res.data.cateno,
+                cname: res.data.cname
+            }
+            console.log(this.Category.cateno)
+            console.log(this.Category.cname)
+           
+        })
+        .catch(err => {
+            console.log(err)
+        })
+     }
+}
+</script>
